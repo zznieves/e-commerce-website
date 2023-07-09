ddaa2a860269eb29a4d2fd1254f34f1390950ad6
Zayon Nieves <zayon.nieves1@gmail.com>
2023-07-09 11:27:37 -0400

    added unique title property to iframe element (interactive map)

--- src/components/Location.js
+++ src/components/Location.js
@@ -26,6 +26,7 @@ const Location = () => {
             <div className="location-map">
                 <iframe
                     id="interactive-map"
+                    title="google-maps-location"
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1614.361446468834!2d-73.97584541310415!3d40.68314157205598!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25bae7e63253b%3A0x2ec74e263fb9805a!2sShake%20Shack%20Flatbush%20-%20Barclays%20Ctr!5e0!3m2!1sen!2sus!4v1688914145987!5m2!1sen!2sus" 
                     style={{border:0}} 
                     allowfullscreen="" 
